21406493e0c751c5d11bbce7d0d42f2b73494219
LastChanceBilly <josetecnom12@hotmail.com>
2018-10-08 14:08:39 -0400

    FunCalc (almost) finished

--- source/utils/FunCalc.py
+++ source/utils/FunCalc.py
@@ -3,56 +3,28 @@ import math
 class FunCalc:
     def __init__(self):
         self.test = 0
+        self.output=  " "
     def calcFunc(self, fx):
-        stack = []
-        output = []
-        def sadd(item):
-            stack.append(item)
+        self.output = " "
         def oadd(item):
-            output.append(item)
+            self.output += item
         if(fx != None):
             for i in fx:
                 val = ord(i)
                 if(val >= 48 and val <= 57):
                     oadd(i)
-                elif((val >= 40 and val <= 47) or (val >= 60 and val <= 62)):
-                    if(val != 44 and val != 46):
-                        if(val == 47):
-                            if(len(stack) > 0):
-                                op = stack[len(stack) -1]
-                                oadd(op)
-                                stack.remove(op)
-                                sadd(i)
-                            else:
-                                sadd(i)
-                        elif(val == 41):
-                            if(len(stack) > 0):
-                                count = 1
-                                for x in stack[:-1]:
-                                    count += 1
-                                    stack[len(stack)-count] = " "
-                                    if(x!="("):
-                                        oadd(stack[len(stack)-count])
-                                bu = []
-                                for i in stack:
-                                    if(i != " "):
-                                        bu.append(i)
-                                stack = []
-                                stack = bu
-                                print(bu)
-                            else:
-                                return "Error, missing ("
-                        else:
-                            sadd(i)
+                elif((val >= 40 and val <= 47) or (val >= 60 and val <= 62) or val == 94):
+                    if(val != 44):
+                        oadd(i)
                     else:
                         return "Error, character "+i+" not defined"
                 else:
                     return "Error, character "+i+" not defined"
-            for i in stack:
-                oadd(i)
-            return(output)
+            val = 0
+            try:
+                val = eval(self.output)
+            except ZeroDivisionError:
+                return "I know it may be hard, but try not to divide by zero pls"
+            return(val)
         else:
             return "Error, no function was provided"
-
-
-
